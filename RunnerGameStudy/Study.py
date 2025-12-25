bc20c871a856dc22eecd32bd788a516c55d59143
Sabeer65 <sabeer.h4774@gmail.com>
2025-12-25 20:53:55 +0530

    Pygame study first commit

---
+++ RunnerGameStudy/Study.py
@@ -0,0 +1,23 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100,200))
+test_surface.fill('Red')
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    screen.blit(test_surface, (0,0))
+
+    pygame.display.update()
+    clock.tick(60)
